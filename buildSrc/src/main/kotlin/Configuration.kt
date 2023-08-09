28a782a6a3203e80d8fa6bbc88f55411f2eb18fd
divyanshupundir <divyanshu.pundir@gmail.com>
2023-08-09 16:21:27 +0530

    chore: Version bump

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -2,6 +2,6 @@ import org.gradle.api.JavaVersion
 
 object Config {
     const val group = "com.divpundir.filetools"
-    const val version = "0.2.0"
+    const val version = "0.2.1"
     val javaVersion = JavaVersion.VERSION_1_8
 }
